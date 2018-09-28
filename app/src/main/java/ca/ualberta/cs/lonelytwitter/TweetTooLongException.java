326743f15d3969f8d5c34451e33d5cbf61819b6c
jialin xie <jialinxie@Jialins-MacBook-Pro.local>
2018-09-28 14:30:05 -0600

    clear button

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/TweetTooLongException.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/TweetTooLongException.java
@@ -2,7 +2,7 @@ package ca.ualberta.cs.lonelytwitter;
 
 public class TweetTooLongException extends Exception{
     TweetTooLongException() {
-        super("The message is too long! Please keep your tweets within 140 charaters");
+        super("The message is too long! Please keep your tweets within 140 characters");
     }
 }
 
