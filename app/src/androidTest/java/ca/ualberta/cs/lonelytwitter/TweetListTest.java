10aa08056d8bebd4cf506375583697368a25ca13
jialin xie <jialinxie@Jialins-MacBook-Pro.local>
2018-10-26 14:11:55 -0600

    lab7 init

--- app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/TweetListTest.java
+++ app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/TweetListTest.java
@@ -16,11 +16,11 @@ public class TweetListTest extends ActivityInstrumentationTestCase2<LonelyTwitte
         //assertTrue(Boolean.FALSE);
         TweetList tweetList = new TweetList();
         Tweet tweet1 = new NormalTweet("added tweet");
-        Tweet tweet2 = new NormalTweet("added tweet");
+        //Tweet tweet2 = new NormalTweet("added tweet");
         tweetList.addTweet(tweet1);
-        tweetList.addTweet(tweet2);
-        assertEquals(1 ,tweetList.getCount());
-        //assertTrue(tweetList.hasTweet(tweet1));
+        //tweetList.addTweet(tweet2);
+        //assertEquals(1 ,tweetList.getCount());
+        assertTrue(tweetList.hasTweet(tweet1));
     }
 
     // JUnit test method for checking whether a tweet is in the TweetList
